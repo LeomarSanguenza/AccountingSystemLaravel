d888cb904135317eb291c3c529c77d9c484a2c16
lsanguenza <leomar@sugartech.ph>
2025-08-05 15:59:52 +0800

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,43 @@
+<head>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+</head>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <form>
+          <!-- Email input -->
+          <div class="mb-4">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" id="email" class="form-control form-control-lg" />
+          </div>
+
+          <!-- Password input -->
+          <div class="mb-4">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" id="password" class="form-control form-control-lg" />
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center mb-4">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="remember" checked />
+              <label class="form-check-label" for="remember"> Remember me </label>
+            </div>
+            <a href="#">Forgot password?</a>
+          </div>
+
+          <!-- Submit button -->
+          <button type="submit" class="btn btn-primary btn-lg w-100">Sign in</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
